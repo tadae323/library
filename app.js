1d170f4770a7533a0c6921c4177081d0f905e831
tadae323 <edyta.lucja@gmail.com>
2021-09-06 20:42:11 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let myLibrary = []
+
+function Book(title, author, pages, isRead) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.isRead = isRead
+  this.info = function() {
+    console.log(`${this.title} by ${this.author}, ${pages} pages, ${this.isRead ? "already read" : "not read yet"}`)
+  }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+theHobbit.info()
