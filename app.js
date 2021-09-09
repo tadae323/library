8703d80b0526ada8828dd8d085e9022120cc8a35
tadae323 <edyta.lucja@gmail.com>
2021-09-09 23:23:51 +0200

    added modal open and close script

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let myLibrary = []
 
+const closeBtn = document.querySelector("#close")
+const modal = document.querySelector("#modal")
+const addBtn = document.querySelector("#add-btn")
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+})
+
+addBtn.addEventListener("click", () => {
+  modal.style.display = "flex"
+})
+
 function Book(title, author, pages, isRead) {
   this.title = title
   this.author = author
@@ -14,5 +26,5 @@ const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
 theHobbit.info()
 
 function addBookToLibrary() {
-  
+  // WIP
 }
