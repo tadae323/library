849b285d35e4a1dd62cc68fe2de61cacd28bb788
tadae323 <edyta.lucja@gmail.com>
2021-09-14 14:33:35 +0200

    drafted a function for adding the book to the library

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ let myLibrary = []
 const closeBtn = document.querySelector("#close")
 const modal = document.querySelector("#modal")
 const addBtn = document.querySelector("#add-btn")
+const saveBtn = document.querySelector("#save")
+const titleInput = document.querySelector("#title")
+const authorInput = document.querySelector("#author")
+const pagesInput = document.querySelector("#pages")
+const finishedCheckbox = document.querySelector("#finished")
 
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
@@ -18,6 +23,8 @@ window.onclick = (event) => {
   }
 }
 
+saveBtn.addEventListener("click", addBookToLibrary)
+
 function Book(title, author, pages, isRead) {
   this.title = title
   this.author = author
@@ -32,5 +39,6 @@ const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
 theHobbit.info()
 
 function addBookToLibrary() {
-  // WIP
+  // create new Book object 
+  // name?
 }
